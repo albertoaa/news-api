20a417c16a41e141256e34f153775bdbe7b61e03
Alberto Aragon Alvarez <ronin8503@gmail.com>
2023-08-21 17:56:04 +0100

    initial query to get headline news

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const axios = require('axios');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const app = express();
 
 const gnewsApiKey = process.env.GNEWS_API_KEY;
+const apiUrl = process.env.API_URL;
 
 app.get('/', (req, res) => {
   res.send('Get all news endpoint');
+});
+
+app.get('/news', async (req, res) => {
+  const headlinesNewsUrl = `${apiUrl}/top-headlines?category=general&apikey=${gnewsApiKey}`;
+  const newsResponse = await axios.get(headlinesNewsUrl);
+  const { data } = newsResponse;
+  res.send(data.articles);
 });
 
 const port = process.env.PORT || 3000;
